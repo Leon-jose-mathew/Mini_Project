226bcac5e07a3ad3e98a8c63c47b7059c577c62f
Bibin Biju <bibinbiju2024@cs.sjcetpalai.ac.in>
2023-05-22 23:27:28 +0530

    first commit

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h2 className="navtitle">People's Ration</h2>
+      <div className="nav-links">
+        <h3 className="navcontent">About</h3>
+        <h3 className="navcontent">Complaint</h3>
+        <h3 className="navcontent">Contact</h3>
+      </div>
+
+      <button className="button type1">
+        <span className="btn-txt">Log In</span>
+      </button>
+
+      
+    </div>
+  );
+}
+
+export default NavBar;
