066662ec8ad8ee7ce82f2020c824921ae44d6fdf
Bibin Biju <bibinbiju2024@cs.sjcetpalai.ac.in>
2023-05-23 11:39:16 +0530

    next commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React, {useState,useEffect} from 'react'
+import React from 'react'
 import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import HomeBody from './Components/HomeBody/HomeBody';
-import GridLoader from "react-spinners/GridLoader";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-   useEffect(() => {
-     setLoading(true);
-     setTimeout(() => {
-       setLoading(false);
-     }, 3000);
-   }, []);
 
   return (
     <div className="App">
-      <div className='loading'>
-        {loading ? (
-          <GridLoader
-            color={"#32cd32"}
-            loading={loading}
-            size={20}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        ) : (
-          <>
-            <HomeBody />
-            <NavBar />
-          </>
-        )}
-      </div>
+      <HomeBody />
+      <NavBar />
     </div>
   );
 }
